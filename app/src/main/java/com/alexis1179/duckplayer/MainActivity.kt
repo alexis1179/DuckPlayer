1e504362e05bfc4a8d4c76acd8cb305e425fa863
alexis1179 <castilloalex6000@gmail.com>
2024-08-30 18:28:41 -0600

    Storage permission

--- app/src/main/java/com/alexis1179/duckplayer/MainActivity.kt
+++ app/src/main/java/com/alexis1179/duckplayer/MainActivity.kt
@@ -1,30 +1,64 @@
 package com.alexis1179.duckplayer
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.rememberNavController
 import com.alexis1179.duckplayer.ui.theme.DuckPlayerTheme
+import com.alexis1179.duckplayer.utils.NavAppHost
 import com.alexis1179.duckplayer.view.components.CustomTopBar
-import com.alexis1179.duckplayer.view.utils.NavAppHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var hasPermission by remember { mutableStateOf(false)}
+            val permissionLauncher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestPermission()) {
+                granted: Boolean ->
+                 hasPermission = granted
+            }
+            LaunchedEffect(Unit) {
+                hasPermission = ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                if(!hasPermission)
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+
             DuckPlayerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize(),
-                    topBar = {CustomTopBar()}) { innerPadding ->
-                    NavAppHost(
-                        navController = rememberNavController(),
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    topBar = { CustomTopBar() }) { innerPadding ->
+                    if (hasPermission) {
+                        NavAppHost(
+                            navController = rememberNavController(),
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    } else {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Text(text = "Permiso no concedido, no puedes acceder al almacenamiento.")
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/alexis1179/duckplayer/view/HomeScreen.kt
+++ app/src/main/java/com/alexis1179/duckplayer/view/HomeScreen.kt
@@ -1,9 +1,15 @@
 package com.alexis1179.duckplayer.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(){
-    Text(text = "Home Screen")
+    Column(modifier = Modifier.padding(4.dp)) {
+        Text(text = "Home Screen")
+    }
 }
